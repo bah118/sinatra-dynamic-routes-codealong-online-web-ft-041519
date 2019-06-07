e49230672b45d94727bd39d2c0e58e37eace72a1
BethAnn Hanlon <herbivore137@gmail.com>
2019-06-07 23:02:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,9 +19,11 @@ class App < Sinatra::Base
   end
   
   get "/multiply" do 
-    @num1 = params[:num1]
-    @num2 = params[:num2]
-    @num1 * @num2
+    # @num1 = params[:num1]
+    # @num2 = params[:num2]
+    # @num1 * @num2
+    
+    params[:num1] * params[:num2]
   end
 
 end
