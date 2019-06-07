59ba141cb23d87fa53a8bb486f93554ab98e4abe
BethAnn Hanlon <herbivore137@gmail.com>
2019-06-07 23:04:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,11 +18,7 @@ class App < Sinatra::Base
     "Goodbye, #{@name}."
   end
   
-  get "/multiply" do 
-    # @num1 = params[:num1]
-    # @num2 = params[:num2]
-    # @num1 * @num2
-    
+  get "/multiply/:num1/:num2" do
     params[:num1] * params[:num2]
   end
 
